4afcfcbff070096e11e9f2ef7697d0c9bd729ed2
Zaiba Iqbal <33644518+zaiba19@users.noreply.github.com>
2019-02-20 23:04:43 -0500

    Update todo.py

--- todo.py
+++ todo.py
@@ -126,7 +126,7 @@ def logout():
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
-	app.run(host="0.0.0.0", port=port, threaded=True, debug=True)
+	app.run(host="0.0.0.0", port=port, threaded=True)
 
 
 
